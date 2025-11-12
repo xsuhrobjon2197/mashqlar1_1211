87b4279a267f4ade307ed5718d511667d7e0305f
xsuhrobjon2197 <xsuhrobjon2197@gmail.com>
2025-11-12 04:56:12 -0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,36 @@
+#1-misol
+i = input("ism kiriting: ")
+
+print(i.upper())
+
+#2-misol
+i = "PythOn So'zi NiMA"
+
+print(i.lower())
+
+#3-misol
+i = input("Parol Kiriting: ")
+
+print(i.lower())
+if i == "admin":
+    print("Havfsiz parol emas")
+
+#4-misol
+i = input("so'z kiritng: ")
+
+print(i.strip())
+
+#5-misol
+i = input("email kiriting: ")
+
+print(i.rstrip())
+
+#6-misol
+i = input("So'z kiriting: ")
+
+print(i.lstrip())
+
+#7-misol
+i = input("So'z kiriting: ")
+
+print(i.rstrip())
